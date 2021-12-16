90142ec64db69730e2de6f5dbc03f3a15ccfac1e
jespe <jesperkauppinen@gmail.com>
2021-12-16 14:23:28 +0200

    + requirements.txt

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from os import path
 
 db = SQLAlchemy()
 login_manager = LoginManager()
