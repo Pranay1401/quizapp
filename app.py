caaf079571fdf87526114a586fcd00a647b16ee3
Jeb <jesperkauppinen@gmail.com>
2022-08-28 16:59:17 +0300

    use production and define port

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from os import environ
+
 from __init__ import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    serve(app, host="0.0.0.0", port=environ.get("BIND_PORT") or 8000)
